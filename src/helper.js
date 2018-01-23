410e06c897779a4737c54939c8e2573785212c27
James Logue <logueja@gmail.com>
2018-01-22 21:22:15 -0700

    add findAverage function to helper, begin compareDistrictAverages

--- src/helper.js
+++ src/helper.js
@@ -19,10 +19,10 @@ export default class DistrictRepository {
         acc[Location.toUpperCase()] = { location: Location.toUpperCase(), data: {} } 
       }
 
-      acc [Location.toUpperCase()].data = { ...acc[Location.toUpperCase()].data, [TimeFrame]: schoolData }
+      acc[Location.toUpperCase()].data = { ...acc[Location.toUpperCase()].data, [TimeFrame]: schoolData }
 
       return acc;
-    }, [])
+    }, {})
   }
 
   findByName(name) {
@@ -46,6 +46,30 @@ export default class DistrictRepository {
     return dataKeys.filter(school => {
       return school.includes(name.toUpperCase());
     }).map( school => this.data[school])
+  }
+
+  findAverage(name) {
+    if (!name) return undefined
+    let schoolDataPoints = Object.values(this.data[name.toUpperCase()].data)
+
+    let number = schoolDataPoints.length
+    let total = schoolDataPoints.reduce((acc, num) => {
+      acc += num
+
+      return acc
+    }, 0)
+
+    return Math.round( (total / number) * 1000)/1000
+
+  }
+
+  compareDistrictAverages(name1, name2) {
+    let school1 = this.findAverage(name1)
+    let school2 = this.findAverage(name2)
+
+    let diff = school1 - school2
+
+    return { [name1.toUpperCase()]: school1, [name2.toUpperCase()]: school2, compared: diff  }
   }
 
 }
