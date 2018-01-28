550379578b701606171b1fcc9e7bd0dcc2adcad7
Melissa Wagasky <melissawagasky@gmail.com>
2018-01-28 10:58:34 -0700

    Add comparison card tests for App.test.js

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import App from './App';
+
 
 describe('App', () => {
 
@@ -21,27 +22,158 @@ describe('App', () => {
     expect(renderedComponent).toMatchSnapshot();
   })
 
-  it('should have a default of an empty state', () => {
+  it('compareison, compareCard1, and compareCard 2 should have a default of an empty state', () => {
     expect(renderedComponent.state().compareCard1).toEqual('');
     expect(renderedComponent.state().compareCard2).toEqual('');
     expect(renderedComponent.state().comparison).toEqual({});
   })
 
-  // it('when handleSearch is called with a location, the state should have that matching data', () => {
-    
+  // (save this test for once we're pulling in all data sets)
 
-  //   matches = 
-  //   { COLORADO: { 
-  //     data: { 2004: 0.24, 2005:0.278 },
-  //     location: "COLORADO"
-  //   }}
-  //   const mockedLocation = 'ASPEN 1'
+  // it('cards should have a default state of the new dataSet', () => {
+  //   const dataSet = { data: 'test data' };
+  //   const mockedMount = jest.fn()
+  //   const renderedComponent = shallow(<App componentDidMount={mockedMount}/>)
 
-  //   renderedComponent.instance().handleSearch(mockedLocation)
-  //   renderedComponent.setState({ cards: []})
-  //   expect(renderedComponent.state().cards).toEqual([matches])
+  //   expect(renderedComponent.state().cards).toEqual({dataSet});
+
   // })
-});
+
+  // the setState in component did mount
+  it('cards should have a default state of 181 districts and 181 cards', () => {
+
+    // expect(renderedComponent.find('.card').length).toEqual(181)
+    // expect(renderedComponent.state().cards.length).toEqual(181);
+
+  })
+
+ 
+
+     // handleSearch
+  // location intake and setState
+
+  it('should take in a location and filter the card set that is rendered from state', () => {
+
+  })
+
+  it('should set compare card1 when a card is selected', () => {
+    const mockedClick = jest.fn()
+    const renderedComponent = mount(<App compareCard={mockedClick}/>)
+
+
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    renderedComponent.find('article').first().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+
+  })
+
+  it('should not compare the same district', () => {
+    const mockedClick = jest.fn()
+    const renderedComponent = mount(<App compareCard={mockedClick}/>)
+
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').first().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').first().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+  })
+
+  it('should set compare card 2 when a second card is selected', () => {
+    const mockedClick = jest.fn()
+    const renderedComponent = mount(<App compareCard={mockedClick}/>)
+
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').first().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').last().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('YUMA SCHOOL DISTRICT 1');
+
+  })
+
+  it('should replace card 2 in state when a 3rd card is selected', () => {
+    const mockedClick = jest.fn()
+    const renderedComponent = mount(<App compareCard={mockedClick}/>)
+
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').at(1).simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('ACADEMY 20');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').at(3).simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('ACADEMY 20');
+    expect(renderedComponent.state().compareCard2).toEqual('ADAMS COUNTY 14');
+
+    renderedComponent.find('article').at(7).simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('ACADEMY 20');
+    expect(renderedComponent.state().compareCard2).toEqual('AGUILAR REORGANIZED 6');
+
+  })
+
+  it('should fire compareDistricts and set state when there are two cards', () => {
+
+  })
+
+  it('should return a comparison ration and two average in the comparison card', () => {
+
+  })
+
+  
+ 
+  it('should set the state of compareCard2 to empty to when the card is selected a second time', () => {
+    const mockedClick = jest.fn()
+    const renderedComponent = mount(<App compareCard={mockedClick}/>)
+
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').first().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').last().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('YUMA SCHOOL DISTRICT 1');
+
+    renderedComponent.find('article').last().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+  })
+
+  it('should set the state of compareCard1 to empty to when the card is selected a second time', () => {
+    const mockedClick = jest.fn()
+    const renderedComponent = mount(<App compareCard={mockedClick}/>)
+
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').first().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('');
+
+    renderedComponent.find('article').last().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('COLORADO');
+    expect(renderedComponent.state().compareCard2).toEqual('YUMA SCHOOL DISTRICT 1');
+
+    renderedComponent.find('article').first().simulate('click')
+    expect(renderedComponent.state().compareCard1).toEqual('');
+    expect(renderedComponent.state().compareCard2).toEqual('YUMA SCHOOL DISTRICT 1');
+
+  })
+})
 
 
 
